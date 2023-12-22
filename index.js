3f4757a949f41e841c944243c7cce70255d0f4df
jose.regalado <jose.regalado@irapuato.gob.mx>
2023-12-22 14:22:33 -0600

    Avance curso

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const routerAPI = require('./routes');
+var cors = require('cors')
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 const validatorHandler = require('./middlewares/validator.handler');
 
 const port = 3003;
 
-
-express.application.prefix = express.Router.prefix = function (path, configure) {
-  var router = express.Router();
-  this.use(path, router);
-  configure(router);
-  return router;
-};
-
 const app = express();
 
 app.use(express.json());
 
+const whitelist = ['http://localhost:8080', 'https://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
