20e63e597dbe4faa6f57a6f0b79ea6b5011bf416
jose.regalado <jose.regalado@irapuato.gob.mx>
2024-02-02 10:59:10 -0600

    Ajuste migración order

--- db/migrations/20240131195338-create-order.js
+++ db/migrations/20240131195338-create-order.js
@@ -1,10 +1,39 @@
 'use strict';
-const { ORDER_TABLE, ORDER_SCHEMA } = require('../models/order.model');
+const { ORDER_TABLE } = require('../models/order.model');
+const { DataTypes } = require('sequelize');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
+const ORDER_PK_NAME = 'id';
+
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, ORDER_SCHEMA);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        field: ORDER_PK_NAME
+      },
+      customerId: {
+        allowNull: true,
+        type: DataTypes.INTEGER,
+        field: 'customer_id',
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'create_at',
+        defaultValue: DataTypes.NOW
+      },
+    });
   },
 
   async down(queryInterface) {
